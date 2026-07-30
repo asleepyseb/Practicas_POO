63a3304f8002cf6da626233fdb70faa2db6742a4
asleepyseb <sebastian26ju@gmail.com>
2026-07-30 14:56:52 -0600

    creacion de menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,52 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion = -1;
+
+        do {
+            System.out.println("\n---- MENU ----");
+            System.out.println("1. Registrar alumno (crear)");
+            System.out.println("2. Imprimir todos los alumnos registrados");
+            System.out.println("3. Actualizar un alumno por matrícula");
+            System.out.println("4. Eliminar un alumno por matrícula");
+            System.out.println("5. Imprimir cuántos hombres y mujeres hay");
+            System.out.println("0. Salir");
+            System.out.print("Elige una opción: ");
+
+            try{
+                // leer opcion como texto
+                opcion = Integer.parseInt(sc.nextLine());
+
+                switch (opcion) {
+                    case 1:
+                        new RegistrarAlumno().ejecutar(sc);
+                        break;
+                    case 2:
+                        new ImprimirAlumno().ejecutar();
+                        break;
+                    case 3:
+                        new ActualizarAlumno().ejecutar(sc);
+                        break;
+                    case 4:
+                        new  EliminarAlumno().ejecutar(sc);
+                        break;
+                    case 5:
+                        new alumnosPS().ejecutar();
+                        break;
+                    case 0:
+                        System.out.println("Saliendo...");
+                        break;
+                    default:
+                        System.out.println("Opcion no valida, intente de nuevo");
+                }
+            }catch(NumberFormatException e){
+                System.out.println("Opcion no valida, intente de nuevo");
+            }
+        } while (opcion != 0);
+        sc.close();
     }
 }
