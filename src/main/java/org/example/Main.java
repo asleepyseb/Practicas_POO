44185cd0eb9542093142178878ae4c7b9dce8c7a
asleepyseb <sebastian26ju@gmail.com>
2026-07-31 13:51:53 -0600

    menu y lista de plataforma creado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,64 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        plataformaStreaming plataforma = new plataformaStreaming();
+        int opcion = 0;
+
+        do {
+            System.out.println("=== MENÚ ===");
+            System.out.println("1. Agregar cuenta");
+            System.out.println("2. Imprimir reporte general");
+            System.out.println("0. Salir");
+
+            try{
+                opcion = Integer.parseInt(sc.nextLine());
+
+                switch(opcion){
+                    case 1:
+                        System.out.println("Ingrese correo electrónico:");
+                        String correoElec = sc.nextLine();
+                        System.out.println("Ingrese los meses a contratar:");
+                        int meses = Integer.parseInt(sc.nextLine());
+
+                        System.out.println("\nSeleccione el plan: \n1. Básico ($5 USD p/mes) \n2. Estándar ($9 USD p/mes) \n3. Premium ($14 USD p/mes + $3 de recargo unico)");
+                        System.out.print("Opción: ");
+                        int tipo = Integer.parseInt(sc.nextLine());
+
+                        planSusc planSeleccionado; //variable de la interf
+                        if(tipo == 1){
+                            planSeleccionado = new planBasico();
+                        } else if(tipo == 2){
+                            planSeleccionado = new planEstandar();
+                        } else if(tipo == 3){
+                            planSeleccionado = new planPremium();
+                        } else{
+                            System.out.println("Opción inválida. Se asignará el plan básico por defecto.");
+                            planSeleccionado = new planBasico();
+                        }
+
+                        //creacion de la cuenta
+                        cuentaUsuario nuevaCuenta = new cuentaSuscrip(correoElec, meses, planSeleccionado);
+                        //registrar la cuenta
+                        plataforma.registrarUsuario(nuevaCuenta);
+                        break;
+                    case 2:
+                        plataforma.imprimirReporte();
+                        break;
+                    case 0:
+                        System.out.println("Saliendo del programa...");
+                        break;
+                    default:
+                        System.out.println("Opción inválida. Intente nuevamente.");
+                }
+            } catch (NumberFormatException e){
+                System.out.println("Error: Por favor ingrese un número correcto.");
+                opcion =-1;
+            }
+        }while(opcion !=0);
+        sc.close();
     }
 }
--- src/main/java/org/example/plataformaStreaming.java
+++ src/main/java/org/example/plataformaStreaming.java
@@ -16,5 +16,32 @@ public class plataformaStreaming {
         cuentas.add(cuenta); // guardar la cuenta
         String sql = "INSERT INTO cuentas_streaming (correo_electronico, meses_activo, plan) VALUES (?, ?, ?)";
 
+        try (Connection conn = ConexionBD.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, cuenta.getCorreoElec());
+            stmt.setInt(2, cuenta.getMesesActivo());
+            stmt.setString(3, cuenta.getPlan().getClass().getSimpleName());
+            stmt.executeUpdate();
+            System.out.println("Usuario registrado con éxito.");
+        } catch (SQLException e) {
+            System.out.println("Error al registrar el usuario: " + e.getMessage());
+        }
+    }
+
+    public void imprimirReporte() {
+        System.out.println("\n---- REPORTE ----");
+        double totalRecaudado = 0.0;
+        if (cuentas.isEmpty()) {
+            System.out.println("No hay cuentas registradas.");
+            return;
+        }
+        for (cuentaUsuario cuenta : cuentas) {
+            double pagar = cuenta.obtenerTotalPagar();
+            totalRecaudado += pagar;
+            String nombrePlan = cuenta.getPlan().getClass().getSimpleName();
+            System.out.println("Usuario: " + cuenta.getCorreoElec() + "\n Plan: " + nombrePlan + "\n Meses activo: " + cuenta.getMesesActivo() + "\n Total a pagar: $" + String.format("%.2f", pagar));
+        }
+        System.out.println("------------------");
+        System.out.println("Total recaudado: $" + String.format("%.2f", totalRecaudado));
     }
 }
