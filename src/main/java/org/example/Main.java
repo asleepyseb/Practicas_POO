eb42c13c3d9dfc57237f4954999daad5ab7ac14e
asleepyseb <sebastian26ju@gmail.com>
2026-08-01 13:56:15 -0600

    menu creado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,68 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Estacionamiento plaza = new  Estacionamiento();
+        int opcion = 0;
+
+        do {
+            System.out.println("\n=== ESTACIONAMIENTO ===");
+            System.out.println("1. Registrar entrada de vehiculo");
+            System.out.println("2. Registrar salida de vehiculo (cobrar)");
+            System.out.println("3. Mostrar Reporte de vehiculos y recaudacion");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opcion: ");
+             try{
+                 opcion = Integer.parseInt(sc.nextLine());
+
+                 switch (opcion) {
+                     case 1:
+                         System.out.println("\n=== REGISTRAR ENTRADA ===");
+                         System.out.print("Ingrese placa del vehiculo: ");
+                         String placa = sc.nextLine();
+                         System.out.println("\n---- TIPOS DE VEHICULO ----");
+                         System.out.println("1. Auto");
+                         System.out.println("2. Moto");
+                         System.out.println("3. Camion");
+                         System.out.print("Seleccione el tipo de vehiculo: ");
+                         int tipo= Integer.parseInt(sc.nextLine());
+
+                         tarifa tarifaElegida;
+                         if(tipo == 2) tarifaElegida = new tarifaMoto();
+                         else if (tipo == 3) tarifaElegida = new tarifaCamion();
+                         else tarifaElegida = new tarifaAuto();
+
+                         vehiculo nuevoAuto = new vehiculoCliente(placa, 0, tarifaElegida);
+                         plaza.registrarEntrada(nuevoAuto);
+                         System.out.println("Vehiculo registrado correctamente.");
+                         break;
+                     case 2:
+                         System.out.println("\n=== REGISTRAR SALIDA ===");
+                         System.out.print("Ingrese placa del vehiculo: ");
+                         String placaSalida = sc.nextLine();
+                         System.out.println("Cuantas horas estuvo estacionado?");
+                         int horas = Integer.parseInt(sc.nextLine());
+
+                         plaza.registrarSalida(placaSalida, horas);
+                         break;
+                     case 3:
+                         plaza.imprimirReporte();
+                         break;
+                     case 0:
+                         System.out.println("Saliendo del programa...");
+                         break;
+                     default:
+                         System.out.println("Opcion invalida. Intente nuevamente.");
+                 }
+             } catch (NumberFormatException e){
+                 System.out.println("Error: Debe ingresar un numero entero. Intente nuevamente.");
+                 opcion = -1;
+             }
+        }while  (opcion != 0);
+
+        sc.close();
     }
 }
