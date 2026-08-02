e12e348ac8766cd65c0b8a094e31677ff23ac34d
asleepyseb <sebastian26ju@gmail.com>
2026-08-02 14:55:58 -0600

    menu creado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,82 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        GestorPaquetes gestor = new  GestorPaquetes();
+        int opcion = -1;
+
+        do {
+            System.out.println("\n=== MENÚ PRINCIPAL ===");
+            System.out.println("1. Registrar paquete");
+            System.out.println("2. Mostrar paquetes");
+            System.out.println("3. Editar paquete (por ID)");
+            System.out.println("4. Eliminar paquete (por ID)");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+
+            try {
+                opcion = Integer.parseInt(sc.nextLine());
+
+                switch (opcion) {
+                    case 1:
+                        System.out.println("\n=== REGISTRAR PAQUETE ===");
+                        System.out.print("Ingresa el destinatario del paquete: ");
+                        String dest =  sc.nextLine();
+                        System.out.print("Ingresa el peso del paquete (Kg): ");
+                        double peso = Double.parseDouble(sc.nextLine());
+
+                        EstrategiaEnvio estrategia = seleccionarEstrategia(sc);
+                        Paquete nuevoPaquete = new Paquete(dest, peso, estrategia);
+                        gestor.registrarPaquete(nuevoPaquete);
+                        break;
+                    case 2:
+                        gestor.mostrarPaquetes();
+                        break;
+                    case 3:
+                        gestor.mostrarPaquetes(); //se muestra la lista para identificar el ID a editar
+                        System.out.println("\n=== EDITAR PAQUETE ===");
+                        int idEditar = Integer.parseInt(sc.nextLine());
+
+                        System.out.println("Nuevo destinatario del paquete: ");
+                        String nuevoDest =   sc.nextLine();
+                        System.out.println("Nuevo peso del paquete (Kg): ");
+                        double nuevoPeso = Double.parseDouble(sc.nextLine());
+                        EstrategiaEnvio nuevaEstrategia = seleccionarEstrategia(sc);
+                        Paquete paqueteEditado = new Paquete(nuevoDest, nuevoPeso, nuevaEstrategia);
+                        gestor.editarPaquete(idEditar, paqueteEditado);
+                        break;
+                    case 4:
+                        System.out.println("\n=== ELIMINAR PAQUETE ===");
+                        System.out.println("Ingresa el ID del paquete a eliminar: ");
+                        int idEliminar = Integer.parseInt(sc.nextLine());
+                        gestor.eliminarPaquete(idEliminar);
+                        break;
+                    case 0:
+                        System.out.println("Saliendo del programa...");
+                        break;
+                    default:
+                        System.out.println("Opción inválida. Intenta nuevamente.");
+                }
+            }catch (NumberFormatException e) {
+                System.out.println("Entrada inválida. Por favor, ingresa un número.");
+            }catch (IllegalArgumentException e) {
+                System.out.println("Error de datos: " + e.getMessage());
+            }
+        } while (opcion != 0);
+        sc.close();
+    }
+
+    // metodo para que el menu de las estrategias no se repita en el main
+    private static EstrategiaEnvio seleccionarEstrategia(Scanner sc) {
+        System.out.println("Tipo de envio: 1. Estandar | 2. Express | 3. Internacional");
+        System.out.println("Selecciona el tipo de envío (1, 2 o 3): ");
+        int tipo = Integer.parseInt(sc.nextLine());
+
+        if (tipo == 2) return new EnvioExpress();
+        if (tipo == 3) return new EnvioInternacional();
+        return new EnvioEstandar(); //opcion por defecto
     }
 }
